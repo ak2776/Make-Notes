d3c6d960830109ac9f4633fca331da802a526406
ak2776 <abhinav.eec@gmail.com>
2022-01-13 21:28:08 +0530

    add title added

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
 
   if (notes == null) {
@@ -12,9 +13,14 @@ addBtn.addEventListener("click", function (e) {
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   // console.log(notesObj);
   showNotes();
 });
@@ -32,8 +38,8 @@ function showNotes() {
     html += `
     <div class="noteCard my-3 mx-3" style="width: 15rem;">
     <div class="card-body">
-      <h5 class="card-title">Note ${index + 1}</h5>
-      <p class="card-text">${element}</p>
+      <h6 class="card-title">${element.title}</h6>
+      <p class="card-text">${element.text}</p>
       <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete note</button>
     </div>
   </div>
